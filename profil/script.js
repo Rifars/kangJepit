486afc89141dbe38f4046e035fd319aea1a96cb3
Rifars <ramlifr12@gmail.com>
2020-09-25 05:53:26 +0700

    menambahkan icon

--- profil/script.js
+++ profil/script.js
@@ -1,7 +1,7 @@
 // tanggal 
 let tanggalLahir=document.querySelector(".tanggal-lahir .tanggalL .tanggal select");
 let tanggal = 1;
-for (tanggal; tanggal <= 30; tanggal++) {
+for (tanggal; tanggal <= 31; tanggal++) {
 	let newTanggal = document.createElement("option");
 	let tanggalNomor = document.createTextNode(tanggal);
 	newTanggal.appendChild(tanggalNomor);
@@ -10,7 +10,7 @@ for (tanggal; tanggal <= 30; tanggal++) {
 
 // tahun
 let tahun = document.querySelector(".tanggal-lahir .tanggalL .tahun select");
-let awalTahun = 1950;
+let awalTahun = 1965;
 for(awalTahun; awalTahun <= 2020; awalTahun++){
 	let tahunBaru = document.createElement("option");
 	let nomorTahun = document.createTextNode(awalTahun);
