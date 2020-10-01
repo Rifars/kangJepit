18a8a9f5ee66cda876d72cec1c06a57cee6496ac
Rifars <ramlifr12@gmail.com>
2020-10-02 06:55:23 +0700

    Memeperbarui codingan di bagian index

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// hamburger button
 let hamburgerButton = document.querySelector(".hamburger-button .button");
  hamburgerButton.addEventListener('click',function(){
  	let hamburgerButtonUL = document.querySelector(".hamburger-button .list");
@@ -14,23 +15,38 @@ let angka = [
 				[3,4,5],
 				[6,7,8]
 ];
+let angkaWarna = [0,1,2];
+angkaWarna.forEach(function(e){
+		angka[e].forEach(function(i) {
+		warna[i].addEventListener('click',function(){
+			img[e].setAttribute('src', 'img/' + warna[i].className + ".png");
+			img[e].classList.add("animasi");
+			setTimeout(function() {
+				img[e].classList.remove("animasi");
+			}, 200);
+		});	
+	});
+});
 
-angka[0].forEach(function(i){
-	warna[i].addEventListener('click',function(){
-		img[0].setAttribute('src', 'img/' + warna[i].className + ".png");
-	});	
-});
-angka[1].forEach(function(i){
-	warna[i].addEventListener('click',function(){
-		img[1].setAttribute('src', 'img/' + warna[i].className + ".png");
-	});	
-});
+// code sebelumnya
+// angka[0].forEach(function(i){
+// 	warna[i].addEventListener('click',function(){
+// 		img[0].setAttribute('src', 'img/' + warna[i].className + ".png");
+// 	});	
+// });
+// angka[1].forEach(function(i){
+// 	warna[i].addEventListener('click',function(){
+// 		img[1].setAttribute('src', 'img/' + warna[i].className + ".png");
+// 	});	
+// });
 
-angka[2].forEach(function(i){
-	warna[i].addEventListener('click',function(){
-		img[2].setAttribute('src', 'img/' + warna[i].className + ".png");
-	});	
-});
+// angka[2].forEach(function(i){
+// 	warna[i].addEventListener('click',function(){
+// 		img[2].setAttribute('src', 'img/' + warna[i].className + ".png");
+// 	});	
+// });
+
+
 
 // sosial media 
 let sosialMedia = document.querySelectorAll(".sosial-media ul li");
